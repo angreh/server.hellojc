7c849083cbf5a12600850881aa5433c66c14525c
piola <piola@192.168.0.156>
2016-06-11 13:41:07 +0200

    new test connection

--- src/main/java/com/phillo/produtos/controller/OlaMundoController.java
+++ src/main/java/com/phillo/produtos/controller/OlaMundoController.java
@@ -4,37 +4,87 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Controller
-public class OlaMundoController
-{
-    @RequestMapping(value = "/olaMundoSpring")
-    public String home()
-    {
-        System.out.print("Executando a lógica co Spring MVC");
-        return "home";
-    }
+public class OlaMundoController {
+	@RequestMapping(value = "/olaMundoSpring")
+	public String home() {
+		System.out.print("Executando a lógica co Spring MVC");
+		return "home";
+	}
 
-    @RequestMapping(value = "/same")
-    public ModelAndView same()
-    {
-        ModelAndView Same = new ModelAndView("same", "name", "Angreh");
+	@RequestMapping(value = "/same")
+	public ModelAndView same() {
+		ModelAndView Same = new ModelAndView("same", "name", "Angreh");
 
-        return Same;
-    }
+		return Same;
+	}
 
-    @RequestMapping(value = "/mapteste")
-    public ModelAndView mapteste()
-    {
-        Map<String, Object> map = new HashMap<>();
-        map.put("nome", "Valor");
-        map.put("nome2", "Valor2");
+	@RequestMapping(value = "/mapteste")
+	public ModelAndView mapteste() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("nome", "Valor");
+		map.put("nome2", "Valor2");
 
-        ModelAndView MapTeste = new ModelAndView("mapteste", map);
+		ModelAndView MapTeste = new ModelAndView("mapteste", map);
 
-        return MapTeste;
-    }
+		return MapTeste;
+	}
+
+	@RequestMapping(value = "/sisardo")
+	public String openConectionBBDD() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// new com.mysql.jdbc.Driver();
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// conn =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/test?user=root&password=");
+			String connectionUrl = "jdbc:mysql://localhost:3306/test";
+			String connectionUser = "piola";
+			String connectionPassword = "";
+			conn = (Connection) DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+			stmt = (Statement) conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM java");
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String firstName = rs.getString("name");
+				String lastName = rs.getString("number");
+				System.out.println("ID: " + id + ", First Name: " + firstName + ", Last Name: " + lastName);
+			}
+			return "hola";
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return "conexao nao feita :(";
+	}
+
 }
